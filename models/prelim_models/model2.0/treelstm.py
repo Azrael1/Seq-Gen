d96401053718b8d0108482760c13ad38d031a13b
Azrael1 <bhavishya.pohani@gmail.com>
2015-06-22 21:25:40 +0530

    Updated the documentation

--- models/prelim_models/model2.0/treelstm.py
+++ models/prelim_models/model2.0/treelstm.py
@@ -30,8 +30,13 @@ def tanh(x):
     return T.tanh(x)
 
 class TreeLSTMLayer(object):
+    """
+    Notations consistent with paper "LSTM over tree structures."
+    """
     def __init__(self, n_in, n_hid):
+        # For Binarized trees
         assert n_hid == n_in/2.0
+        # Input should be of shape (vec_dim, n_in)
         self.input = assert_op(input, input.shape[1] == len(n_in))
         self.n_in = n_in
         self.n_hid = n_hid
@@ -63,6 +68,10 @@ class TreeLSTMLayer(object):
                        self.W_hoL, self.W_hoR, self.W_co, self.bi, self.bfl, self.bfr, self.bx, self.bo]
 
     def fwd(self, h_tm1, c_tm1):
+        """
+        Computes a single forward pass of the layer.
+        """
+        # Reshape the input to 2 parts: namely left and right and do the computation.
         h_tm1L, h_tm1R = T.reshape(h_tm1.T, (self.n_in/2, 2*vec_dim))[:, :vec_dim].T, \
                          T.reshape(h_tm1.T, (self.n_in/2, 2*vec_dim))[:, vec_dim:2*vec_dim].T
         c_tm1L, c_tm1R = T.reshape(c_tm1.T, (self.n_in/2, 2*vec_dim))[:, :vec_dim].T, \
@@ -81,9 +90,13 @@ class TreeLSTMLayer(object):
         return h_t, c_t, self.n_hid
 
 class TreeLSTM(object):
+    """
+    Build a tree structured LSTM with each layer being an instance of class TreeLSTMlayer.
+    """
     # Builds a tree structured LSTM based on the sentence length. Does not have paragraph support for now.
     # Does not have parse structured tree support. Works on 2**x (x is integer) length of sentences.
     def __init__(self, sen_vec):
+        # sen_vec is a shared variable of shape (vec_dim, sentence_length)
         self.sen_vec = assert_op(sen_vec, sen_vec.shape[0] == 50)
         n_layers = T.log2(sen_vec.shape[1])
         self.n_in = sen_vec.shape[1]
@@ -91,6 +104,9 @@ class TreeLSTM(object):
         self.params = []
 
     def tree_output(self):
+        """
+        Compute the output layerwise and feed it to the next layer.
+        """
         n_t0 = self.n_in
         h_t0 = self.sen_vec
         c_t0 = T.zeros_like(h_t0)
@@ -102,6 +118,8 @@ class TreeLSTM(object):
         n_hid = T.cast(nin_tm1/2.0, 'float32')
         layer = TreeLSTMLayer(nin_tm1, n_hid)
         self.params.append(layer.params)
+        # The input is padded with zeros to keep a consistent shape over the scan recurrence.
+        # So take out the part that is needed. Do your magic, again pad with zeros and return it.
         h_t, c_t, n_inx = layer.fwd(h_tm1[:, :nin_tm1], c_tm1[:, nin_tm1])
         h_tx = T.set_subtensor(T.zeros(vec_dim, self.n_in), h_t)
         c_tx = T.set_subtensor(T.zeros(vec_dim, self.n_in), c_t)
