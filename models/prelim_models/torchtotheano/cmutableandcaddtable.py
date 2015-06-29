b3d76321ccef40730cac595d29388f825f8eaec0
Azrael1 <bhavishya.pohani@gmail.com>
2015-06-29 15:08:47 +0530

    Updated the documentation

--- models/prelim_models/torchtotheano/cmutableandcaddtable.py
+++ models/prelim_models/torchtotheano/cmutableandcaddtable.py
@@ -11,8 +11,10 @@ def CMulTensors(tensor1, tensor2):
     :param tensor2: same
     :return: same
     """
+    # Implement check that the tensors hold the same number of elements.
     tensor1_ = assert_op(tensor1, tensor1.shape[0]*tensor1.shape[1]==tensor2.shape[0]*tensor2.shape[1])
-    tensor2_ = T.reshape(tensor1_, (tensor1_.shape[0], tensor1_.shape[1]), ndim =2)
+    # Reshape into shape of tensor1
+    tensor2_ = T.reshape(tensor2, (tensor1_.shape[0], tensor1_.shape[1]), ndim =2)
     return tensor1_*tensor2_
 
 
@@ -23,7 +25,9 @@ def CAddTensors(tensor1, tensor2):
     :param tensor2: same
     :return: same
     """
+    # Implement check that the tensors hold the same number of elements.
     tensor1_ = assert_op(tensor1, tensor1.shape[0]*tensor1.shape[1]==tensor2.shape[0]*tensor2.shape[1])
-    tensor2_ = T.reshape(tensor1_, (tensor1_.shape[0], tensor1_.shape[1]), ndim =2)
+    # Reshape into shape of tensor1
+    tensor2_ = T.reshape(tensor2, (tensor1_.shape[0], tensor1_.shape[1]), ndim =2)
     return tensor1_+tensor2_
 
